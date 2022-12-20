755a8b34b9a8e88ba9e5f5dc5564a7d517707ebd
Srikanthnaid <ksru044gmail.com>
2022-12-20 16:08:42 +0530

    File io added

---
+++ lib/src/main/java/Day27AddressBook/JavaFile.java
@@ -0,0 +1,71 @@
+package Day27AddressBook;
+
+public class JavaFile {
+	/**
+	 * private variables can only be accessed within the same class (an outside
+	 * class has no access to it) private = restricted access However, it is
+	 * possible to access them if we provide public get and set methods.
+	 */
+	static int id = JavaFileMain.people.size();
+	private String Firstname;
+	private String Lastname;
+	private String phoneNumber;
+	private String email;
+	private String address;
+
+	/**
+	 * creating a parameterized constructor of ContactDetails by passing parameters
+	 * with no return type
+	 * 
+	 * @param firstName
+	 * @param lastName
+	 * @param address
+	 * @param phonenumber
+	 * @param email
+	 * 
+	 */
+	JavaFile(String name, String surname, String phoneNumber, String email, String address) {
+		this.Firstname = Firstname;
+		this.Lastname = Lastname;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.address = address;
+		id++;
+	}
+
+	/*
+	 * Getters and Setters
+	 * 
+	 */
+
+	String getFirstName() {
+		return Firstname;
+	}
+
+	String getLastName() {
+		return Lastname;
+	}
+
+	String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	String getEmail() {
+		return email;
+	}
+
+	String getAddress() {
+		return address;
+	}
+
+	/*
+	 * To string for the dispaly pattern
+	 * 
+	 */
+	@Override
+	public String toString() {
+		return "\n\nfirstName: " + getFirstName() + "\nlastname: " + getLastName() + "\nPhone number: "
+				+ getPhoneNumber() + "\nEmail: " + getEmail() + "\nAddress: " + getAddress();
+	}
+
+}
